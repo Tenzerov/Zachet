2ff56d84e7f31fab67adac9bafe41e7dfa178d87
Tenzerov <milchakoff.sergei@yandex.ru>
2024-08-19 09:02:16 +0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-# create network
-docker network create zachet
+# Переменные
+NETWORK_NAME="zachet-network"
+DB_CONTAINER_NAME="myapp-db"
+WEB_CONTAINER_NAME="myapp-web"
+DB_IMAGE="postgres:16.4"
+WEB_IMAGE="nginx:1.26"
+DB_PASSWORD="123"
+DB_NAME="polzakdb"
+DB_USER="polzak"
 
-# run container
-docker run -d --name webapp --network zachet nginx
-docker run -d --name database --network zachet postgres
-chmod +x .sh
+# Создание отдельной сети
+docker network create $NETWORK_NAME
+
+# Запуск контейнера базы данных
+docker run -d \
+    --name $DB_CONTAINER_NAME \
+    --network $NETWORK_NAME \
+    -e POSTGRES_PASSWORD=$DB_PASSWORD \
+    -e POSTGRES_USER=$DB_USER \
+    -e POSTGRES_DB=$DB_NAME \
+    $DB_IMAGE
+
+# Запуск контейнера веб-приложения
+docker run -d \
+    --name $WEB_CONTAINER_NAME \
+    --network $NETWORK_NAME \
+    -p 8080:80 \
+    $WEB_IMAGE
+
+echo "Веб-приложение запущено на http://localhost:8080"
