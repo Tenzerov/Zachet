f7e772f22354e51f82375e680b4495aefe1c5cdb
Tenzerov <milchakoff.sergei@yandex.ru>
2024-08-19 09:45:56 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Переменные
-NETWORK_NAME="zachet-network"
-DB_CONTAINER_NAME="zachet-db"
-WEB_CONTAINER_NAME="zachet-web"
+NETWORK="zachet-network"
+DB_CONTAINER="zachet-db"
+WEB_CONTAINER="zachet-web"
 DB_IMAGE="postgres:16.4"
 WEB_IMAGE="nginx:1.26"
 DB_PASSWORD="123"
@@ -11,12 +11,12 @@ DB_NAME="polzakdb"
 DB_USER="polzak"
 
 # Создание отдельной сети
-docker network create $NETWORK_NAME
+docker network create $NETWORK
 
 # Запуск контейнера базы данных
 docker run -d \
-    --name $DB_CONTAINER_NAME \
-    --network $NETWORK_NAME \
+    --name $DB_CONTAINER \
+    --network $NETWORK \
     -e POSTGRES_PASSWORD=$DB_PASSWORD \
     -e POSTGRES_USER=$DB_USER \
     -e POSTGRES_DB=$DB_NAME \
@@ -24,8 +24,8 @@ docker run -d \
 
 # Запуск контейнера веб-приложения
 docker run -d \
-    --name $WEB_CONTAINER_NAME \
-    --network $NETWORK_NAME \
+    --name $WEB_CONTAINER \
+    --network $NETWORK \
     -p 8080:80 \
     $WEB_IMAGE
 
