81feec63d901bbb08edd6ddab61bb1383346855a
Tenzerov <milchakoff.sergei@yandex.ru>
2024-08-19 16:11:55 +0400

    Delete bash.sh

--- bash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# create network
-docker network create zachet
-
-# run container
-docker run -d --name webapp --network zachet nginx
-docker run -d --name database --network zachet postgres
-chmod +x .sh
